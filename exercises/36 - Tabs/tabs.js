b7eabc6c9d640b07dd28d7277dc032c1fddd02fb
Amory <108595086+amorychambers@users.noreply.github.com>
2022-12-05 20:04:38 +0000

    Completed Tabs exercise

--- exercises/36 - Tabs/tabs.js
+++ exercises/36 - Tabs/tabs.js
@@ -1,1 +1,16 @@
-console.log('ya ya wes we get it.. IT WORKS!');
+/* eslint-disable */
+
+const tabs = document.querySelector('.tabs');
+const tabButtons = tabs.querySelectorAll('[role="tab"]');
+const tabPanels = tabs.querySelectorAll('[role="tabpanel"]');
+
+function handleTabClick(e){
+    tabPanels.forEach(panel => (panel.hidden = true));
+    tabButtons.forEach(button => (button.ariaSelected = "false"));
+    e.currentTarget.ariaSelected = "true";
+    const { id } = e.currentTarget;
+    const selectedPanel = tabs.querySelector(`[aria-labelledby=${id}]`);
+    selectedPanel.hidden = false;
+}
+
+tabButtons.forEach(button => button.addEventListener('click', handleTabClick));
